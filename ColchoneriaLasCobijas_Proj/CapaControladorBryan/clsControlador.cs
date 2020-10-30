0b03470b98f85bf75d70340abe3347092822749c
Colossatrox <bryanorlando-98@hotmail.com>
2020-10-29 20:37:06 -0600

    Cuentas Contables

---
+++ ColchoneriaLasCobijas_Proj/CapaControladorBryan/clsControlador.cs
@@ -0,0 +1,52 @@
+﻿using CapaModeloBryan;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControladorBryan
+{
+    public class clsControlador
+    {
+        clsSentencias Sn = new clsSentencias();
+        //Funcion para obtener los datos que se van a mostrar en el treeview y pasarlos a la capa vista
+        public DataSet funcLlenarTree()
+        {
+            OdbcDataAdapter Dt = Sn.funcLlenarTree();
+            DataSet Table = new DataSet();
+            Dt.Fill(Table,"cuenta_contable");
+            return Table;
+        }
+
+        //Funcion para obtener el codigo cuando se seleccciona una cuenta del treeview y pasarlo a la capa vista
+        public int funcCodigoMax(int Codigo)
+        {
+            int CodigoNuevo = Sn.funcInsertar(Codigo);
+            return CodigoNuevo;
+        }
+
+        //Funcion para obtener el codigo cuando no se seleccciona una cuenta del treeview y pasarlo a la capa vista
+        public int funcCodigoMaxNivel0()
+        {
+            int CodigoNuevo = Sn.funcInsertarNivel0();
+            return CodigoNuevo;
+        }
+
+        //Funcion para mandar los datos para ejecutar la consulta de agregar una nueva cuenta contable y devolver la respuesta a la capa vista
+        public bool funcAgregar(int Codigo, string Nombre, int Nivel, int Padre, int TipoCuenta)
+        {
+            bool Respuesta = Sn.funcAgregar(Codigo, Nombre, Nivel, Padre, TipoCuenta);
+            return Respuesta;
+        }
+
+        //Funcion para obtener los items con los que se van a llenar los combobox del form
+        public string[] funcItems(string Tabla, string Campo1)
+        {
+            string[] Items = Sn.funcLlenarCmb(Tabla, Campo1);
+            return Items;
+        }
+    }
+}
---
+++ ColchoneriaLasCobijas_Proj/CapaVistaBryan/Mantenimientos/frmCuentas.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CapaVistaBryan.Mantenimientos
+{
+    partial class frmCuentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCuentas));
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.tvwCuentas = new System.Windows.Forms.TreeView();
+            this.txtNombreCuenta = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.cmbTipoCuenta = new System.Windows.Forms.ComboBox();
+            this.lblTipoCuenta = new System.Windows.Forms.Label();
+            this.cmbIdTipoCuenta = new System.Windows.Forms.ComboBox();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(652, 101);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(88, 25);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // tvwCuentas
+            // 
+            this.tvwCuentas.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tvwCuentas.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tvwCuentas.Location = new System.Drawing.Point(0, 146);
+            this.tvwCuentas.Name = "tvwCuentas";
+            this.tvwCuentas.Size = new System.Drawing.Size(834, 365);
+            this.tvwCuentas.TabIndex = 11;
+            // 
+            // txtNombreCuenta
+            // 
+            this.txtNombreCuenta.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreCuenta.Location = new System.Drawing.Point(267, 57);
+            this.txtNombreCuenta.Name = "txtNombreCuenta";
+            this.txtNombreCuenta.Size = new System.Drawing.Size(338, 25);
+            this.txtNombreCuenta.TabIndex = 0;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(180, 65);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(75, 17);
+            this.lblNombre.TabIndex = 9;
+            this.lblNombre.Text = "NOMBRE:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(351, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(83, 19);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "CUENTAS";
+            // 
+            // cmbTipoCuenta
+            // 
+            this.cmbTipoCuenta.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipoCuenta.FormattingEnabled = true;
+            this.cmbTipoCuenta.Location = new System.Drawing.Point(267, 101);
+            this.cmbTipoCuenta.Name = "cmbTipoCuenta";
+            this.cmbTipoCuenta.Size = new System.Drawing.Size(338, 27);
+            this.cmbTipoCuenta.TabIndex = 2;
+            this.cmbTipoCuenta.SelectedIndexChanged += new System.EventHandler(this.cmbTipoCuenta_SelectedIndexChanged);
+            // 
+            // lblTipoCuenta
+            // 
+            this.lblTipoCuenta.AutoSize = true;
+            this.lblTipoCuenta.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoCuenta.Location = new System.Drawing.Point(22, 109);
+            this.lblTipoCuenta.Name = "lblTipoCuenta";
+            this.lblTipoCuenta.Size = new System.Drawing.Size(233, 19);
+            this.lblTipoCuenta.TabIndex = 14;
+            this.lblTipoCuenta.Text = "TIPO DE CUENTA CONTABLE:";
+            // 
+            // cmbIdTipoCuenta
+            // 
+            this.cmbIdTipoCuenta.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbIdTipoCuenta.FormattingEnabled = true;
+            this.cmbIdTipoCuenta.Location = new System.Drawing.Point(611, 101);
+            this.cmbIdTipoCuenta.Name = "cmbIdTipoCuenta";
+            this.cmbIdTipoCuenta.Size = new System.Drawing.Size(35, 27);
+            this.cmbIdTipoCuenta.TabIndex = 15;
+            this.cmbIdTipoCuenta.Visible = false;
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.BackColor = System.Drawing.Color.Transparent;
+            this.btnAyuda.FlatAppearance.BorderSize = 0;
+            this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyuda.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAyuda.Image = ((System.Drawing.Image)(resources.GetObject("btnAyuda.Image")));
+            this.btnAyuda.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAyuda.Location = new System.Drawing.Point(12, 12);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(88, 58);
+            this.btnAyuda.TabIndex = 16;
+            this.btnAyuda.Text = "AYUDA";
+            this.btnAyuda.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAyuda.UseVisualStyleBackColor = false;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
+            // 
+            // frmCuentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(834, 511);
+            this.Controls.Add(this.btnAyuda);
+            this.Controls.Add(this.cmbIdTipoCuenta);
+            this.Controls.Add(this.lblTipoCuenta);
+            this.Controls.Add(this.cmbTipoCuenta);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tvwCuentas);
+            this.Controls.Add(this.txtNombreCuenta);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTitulo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCuentas";
+            this.Text = "Cuentas Contables";
+            this.Load += new System.EventHandler(this.frmCuentas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TreeView tvwCuentas;
+        private System.Windows.Forms.TextBox txtNombreCuenta;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ComboBox cmbTipoCuenta;
+        private System.Windows.Forms.Label lblTipoCuenta;
+        private System.Windows.Forms.ComboBox cmbIdTipoCuenta;
+        private System.Windows.Forms.Button btnAyuda;
+    }
+}
